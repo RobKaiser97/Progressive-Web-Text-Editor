430252b66c09d785d25439eb9cb0a22f5ee10703
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-12 18:41:40 -0500

    Update install.js with PWA installation logic

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,25 +1,44 @@
-const butInstall = document.getElementById("buttonInstall");
+const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-window.addEventListener("beforeinstallprompt", (event) => {
-  // Store the triggered events
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  event.preventDefault();
+  // Stash the event so it can be triggered later.
   window.deferredPrompt = event;
+  // Update UI notify the user they can install the PWA
+  butInstall.style.display = 'block';
 });
 
-butInstall.addEventListener("click", async () => {
-  const promptEvent = window.deferredPrompt;
-  if (!promptEvent) {
-    return;
+butInstall.addEventListener('click', () => {
+  // Show the prompt if deferredPrompt is stored
+  if (window.deferredPrompt) {
+    window.deferredPrompt.prompt();
+    window.deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      window.deferredPrompt = null;
+      butInstall.style.display = 'none';
+    });
+  } else {
+    // The user hit the install button but the app can't prompt to install (likely already installed)
+    alert('The app is already installed or not available for install.');
   }
+});
 
-  // Show prompt
-  promptEvent.prompt();
-
-  // Reset the deferred prompt variable
+window.addEventListener('appinstalled', (event) => {
+  // Log the installation to analytics or console
+  console.log('PWA was installed', event);
+  // Hide the install button after the app is installed
+  butInstall.style.display = 'none';
+  // Clear any stored deferredPrompt since the app was installed
   window.deferredPrompt = null;
 });
 
-window.addEventListener("appinstalled", (event) => {
-  // Clear prompt
-  window.deferredPrompt = null;
+// On load, or when the 'load' event fires, check if the PWA is already installed.
+window.addEventListener('load', () => {
+  butInstall.style.display = 'block'; // Always show the install button until app is installed
 });
