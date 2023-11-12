c90e06191f04e52a51ba98190d2effe37386f2e3
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-12 12:24:21 -0500

    Add HtmlWebpackPlugin, InjectManifest, and WebpackPwaManifest plugins to webpack config.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,13 +18,58 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        filename: "index.html",
+        title: "JATE",
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        background_color: '#708090',
+        start_url: '/',
+        theme_color: '#FF0000',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/img/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime"
+              ],
+            },
+          },
+        }
       ],
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
     },
   };
 };
