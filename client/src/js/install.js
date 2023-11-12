5a409372c629433a5c7eec1cf3cd778270ca1e18
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-12 18:16:36 -0500

    Refactor install.js to use consistent quotes and handle deferred prompt correctly

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,22 +1,25 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-window.addEventListener('beforeinstallprompt', (event) => {
-  event.preventDefault();
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Store the triggered events
   window.deferredPrompt = event;
 });
 
-butInstall.addEventListener('click', async () => {
-  defferedPrompt.prompt();
-  const { outcome } = await defferedPrompt.userChoice;
-  if (outcome === 'accepted') {
-    console.log('User accepted the A2HS prompt');
-  } else {
-    console.log('User dismissed the A2HS prompt');
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
   }
+
+  // Show prompt
+  promptEvent.prompt();
+
+  // Reset the deferred prompt variable
   window.deferredPrompt = null;
 });
 
-window.addEventListener('appinstalled', (event) => {
-  console.log('App installed');
+window.addEventListener("appinstalled", (event) => {
+  // Clear prompt
+  window.deferredPrompt = null;
 });
